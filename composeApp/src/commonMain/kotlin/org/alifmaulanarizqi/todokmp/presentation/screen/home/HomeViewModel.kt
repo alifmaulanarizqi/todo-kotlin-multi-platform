0560399aca5fa30e4b0eb46060d80e1d8ae05b61
alif <alifmaulana312@gmail.com>
2026-02-28 15:38:35 +0700

    feat: impelement search query

--- composeApp/src/commonMain/kotlin/org/alifmaulanarizqi/todokmp/presentation/screen/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/alifmaulanarizqi/todokmp/presentation/screen/home/HomeViewModel.kt
@@ -2,7 +2,9 @@ package org.alifmaulanarizqi.todokmp.presentation.screen.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import org.alifmaulanarizqi.todokmp.data.ToDoRepository
 import org.alifmaulanarizqi.todokmp.domain.ToDoTask
@@ -11,12 +13,33 @@ import org.alifmaulanarizqi.todokmp.util.RequestState
 class HomeViewModel(
     private val repository: ToDoRepository
 ): ViewModel() {
-    val tasks = repository.readAllTasks()
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = RequestState.Loading
-        )
+    private var _searchQuery = MutableStateFlow<String?>(null)
+    val searchQuery = _searchQuery
+
+    val tasks = combine(
+        repository.readAllTasks(),
+        _searchQuery
+    ) { tasks, query ->
+        when(tasks) {
+            is RequestState.Success -> {
+                val filteredTasks = tasks.data.let { list ->
+                    query?.let {
+                        if(query.isBlank()) list
+                        else list.filter { task ->
+                            task.title.contains(query, ignoreCase = true)
+                        }
+                    } ?: list
+                }.sortedByDescending { it.priority.ordinal }
+
+                RequestState.Success(filteredTasks)
+            }
+            else -> tasks
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = RequestState.Loading
+    )
 
     fun markTestAsCompleted(task: ToDoTask): RequestState<Unit> {
         return repository.updateTask(task)
@@ -24,5 +47,9 @@ class HomeViewModel(
 
     fun removeTask(taskId: String): RequestState<Unit> {
         return repository.removeTask(taskId)
+    }
+
+    fun updateSearchQuery(value: String) {
+        _searchQuery.value = value
     }
 }
